8a5b67bfa6c7dd4fa3f599127af450a9bb8963df
XiangYu <kisky3@126.com>
2022-01-08 16:58:10 +0900

    add axios

--- github-search/src/components/Search/index.jsx
+++ github-search/src/components/Search/index.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './index.css';
+import axios from 'axios';
 
 class Search extends Component {
+    search = () => {
+        // get user's input, constructure and rename var
+        const {keyWordElement:{value: keyWord}} = this;
+        
+        // send request
 
+    }
     render() {
         return (
             <div className="search">
-                <input className="search-input" type="text" onChange={this.handleValue} placeholder="search github user here.."/>
-                <button className="search-button"> search</button>
+                <input className="search-input" type="text" ref={c => this.keyWordElement = c} placeholder="search github user here.."/>
+                <button className="search-button" onClick={this.search}> search</button>
             </div>
         );
     }
