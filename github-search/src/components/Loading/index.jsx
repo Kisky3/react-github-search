deb923c504444d80eb3d985d08281f9e45c98023
XiangYu <kisky3@126.com>
2022-01-08 17:22:34 +0900

    add loading UI

--- github-search/src/components/Loading/index.jsx
+++ github-search/src/components/Loading/index.jsx
@@ -1,14 +1,18 @@
 import Loader from "react-loader-spinner";
 import { Component } from 'react';
+import './index.css'
 export default class Loading extends Component {
     render() {
         return (
-            <Loader
-                type="Puff"
-                color="#ccc"
-                height={100}
-                width={100}
-            />
+            <div className="loading">
+                <Loader
+                    type="Puff"
+                    color="#ccc"
+                    height={100}
+                    width={100}
+                    className="loading-mark"
+                />
+            </div>
         );
     }
 }
