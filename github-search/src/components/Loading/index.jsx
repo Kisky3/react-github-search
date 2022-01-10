8c895162481791a9862eb8a377b7b45bc5e1a6ae
XiangYu <kisky3@126.com>
2022-01-10 21:47:28 +0900

    add loading mark style

--- github-search/src/components/Loading/index.jsx
+++ github-search/src/components/Loading/index.jsx
@@ -5,14 +5,18 @@ export default class Loading extends Component {
     render() {
         const {showLoading} = this.props
         return (
-            <div className="loading" style={{display: showLoading ? 'block': 'none'}}>
+            <div style={{display: showLoading ? 'block': 'block'}} className="loading">
+            <div>
                 <Loader
                     type="Puff"
                     color="#ccc"
                     height={100}
                     width={100}
                     className="loading-mark"
+                    timeout={10000}
                 />
+            </div>
+            <span className="loading-text">Loading</span>
             </div>
         );
     }
