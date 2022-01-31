358f1881da111440bbd4c65ee7938b0f3608be21
XiangYu <kisky3@126.com>
2022-02-01 00:06:58 +0900

    add background

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,37 @@ class App extends Component {
 
   render() {
     const {users,showLoading } = this.state
+    let config = {
+      num: [4, 7],
+      rps: 0.1,
+      radius: [5, 40],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-40, 40],
+      alpha: [0.6, 0],
+      scale: [1, 0.1],
+      position: "center", 
+      color: ["random", "#ff0000"],
+      cross: "dead",
+      random: 15,
+      g: 5,
+      onParticleUpdate: (ctx, particle) => {
+          ctx.beginPath();
+          ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+          ctx.fillStyle = particle.color;
+          ctx.fill();
+          ctx.closePath();
+      }
+    };
     return (
+      <div>
+      <ParticlesBg type="circle" bg={true} config={config}/>
       <div className="container">
         <header>Search Github Users</header>
-        <ParticlesBg type="circle" bg={true} />
         <Loading showLoading={showLoading} />
         <Search getUserData={this.getUserData} setLoadingState={this.setLoadingState} />
         <List users={users}/>
+      </div>
       </div>
     );
   }
